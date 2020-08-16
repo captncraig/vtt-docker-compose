7b409b643d1b88b1315465abb9eeeb07d20393d9
Craig Peterson <192540+captncraig@users.noreply.github.com>
2020-08-15 21:44:12 -0400

    hosts

--- foundry.sh
+++ foundry.sh
@@ -2,12 +2,15 @@
 
 docker stop foundry || true && docker rm foundry || true
 
+# todo: make a host list in the env file and launch an instance of foundry per host
 source .env
 
 docker run \
   --name foundry \
   --env FOUNDRY_USERNAME=$FOUNDRY_USER \
   --env FOUNDRY_PASSWORD=$FOUNDRY_PASS \
+  --env "VIRTUAL_HOST=vtt.captncraig.io" \
+  --env "LETSENCRYPT_HOST=vtt.captncraig.io" \
   --publish 30000:30000/tcp \
   --volume /data:`pwd`/data \
   felddy/foundryvtt:release
