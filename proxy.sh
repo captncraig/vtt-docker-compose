830710db1c8ecd6fce979378aea88a6b96feabab
Craig Peterson <192540+captncraig@users.noreply.github.com>
2020-08-15 21:34:43 -0400

    initial

---
+++ proxy.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+docker stop nginx-proxy || true && docker rm nginx-proxy || true
+
+docker run --detach \
+    --name nginx-proxy \
+    --publish 80:80 \
+    --publish 443:443 \
+    --volume /etc/nginx/certs \
+    --volume /etc/nginx/vhost.d \
+    --volume /usr/share/nginx/html \
+    --volume /var/run/docker.sock:/tmp/docker.sock:ro \
+    jwilder/nginx-proxy
+
+docker stop nginx-proxy-letsencrypt || true && docker rm nginx-proxy-letsencrypt || true
+
+docker run --detach \
+    --name nginx-proxy-letsencrypt \
+    --volumes-from nginx-proxy \
+    --volume /var/run/docker.sock:/var/run/docker.sock:ro \
+    --env "DEFAULT_EMAIL=mail@yourdomain.tld" \
+    jrcs/letsencrypt-nginx-proxy-companion
---
+++ readme.md
@@ -0,0 +1,11 @@
+# Docker setup to install foundry VTT on a servier with docker
+
+## 0: Prerequisites
+
+1. Install docker
+2. Clone this repo 
+3. Setup secrets
+  - `cp .env.example .env`
+  - `vi .env`
+  - put in your unique values
+
